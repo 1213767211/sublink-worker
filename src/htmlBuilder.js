02ae3acc59c50ad1bba998d0d9e4ad22d89e99ca
Master <131366923+1213767211@users.noreply.github.com>
2025-01-16 15:09:45 +0800

    Update htmlBuilder.js

--- src/htmlBuilder.js
+++ src/htmlBuilder.js
@@ -62,7 +62,7 @@ const generateDarkModeToggle = () => `
 `;
 
 const generateGithubLink = () => `
-  <a href="https://github.com/7Sageer/sublink-worker" target="_blank" rel="noopener noreferrer" class="github-link">
+  <a href="https://github.com/1213767211/sublink-worker" target="_blank" rel="noopener noreferrer" class="github-link">
     <i class="fab fa-github"></i>
   </a>
 `;
